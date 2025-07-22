8b4f3b0ac8638396770513d410fac91583aa5c28
themixednuts <jonfonts@gmail.com>
2025-07-22 01:42:34 -0500

    dep: bump

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,14 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter({
+			fallback: 'index.html'
+		})
+	},
+	compilerOptions: {
+		experimental: { async: true }
+	}
 };
 
 export default config;
